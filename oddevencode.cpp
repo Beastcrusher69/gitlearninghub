d4154f03660f02324c123e80cf50694a798a83bc
Jay <jaykapadia389@gmail.com>
2022-11-09 17:50:47 +0530

    fresh code

---
+++ oddevencode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+
+    cout<<"enter a number:";
+    cin>> i;
+
+    if(i%2==0)
+    {
+        cout<<"given number is odd";
+    }
+    else
+    {
+        cout<<"given number is evem";
+    }
+}
